93da3b518368861d2e9b3aa037ff57665fcae49a
ENZO XAVIER BISCAIA <enzo.biscaia@cs.up.edu.br>
2025-08-29 11:32:57 -0300

    Criação da API e arquivo de teste

--- Ecommerce/API/Program.cs
+++ Ecommerce/API/Program.cs
@@ -1,6 +1,12 @@
+Console.Clear();
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//Funcionalidades - Requisições
+//Endereço URL
+//Método HTTP
+app.MapGet("/", () => "Minha API");
+app.MapGet("/funcionalidade", () => "Segunda funcionalidade");
+app.MapPost("/funcionalidade", () => "Funcionalidade com Post");
 
 app.Run();
