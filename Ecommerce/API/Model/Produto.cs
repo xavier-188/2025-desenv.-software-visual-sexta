fb8e162c4ddedbf28a464f2d2b5ab5d437611141
ENZO XAVIER BISCAIA <enzo.biscaia@cs.up.edu.br>
2025-09-12 11:25:19 -0300

    Funcionalidades Buscar e Cadastrar

--- Ecommerce/API/Model/Produto.cs
+++ Ecommerce/API/Model/Produto.cs
@@ -3,13 +3,15 @@ namespace API.Models;
 
 public class Produto
 {
+
+    //Construtor
     public Produto()
     {
         Id = Guid.NewGuid().ToString();
         CriadoEm = DateTime.Now;
     }
     public string Id { get; set; }
-    public string Nome { get; set; }
+    public string? Nome { get; set; }
     public int Quantidade { get; set; }
     public double Preco { get; set; }
     public DateTime CriadoEm { get; set; }
--- Ecommerce/API/Program.cs
+++ Ecommerce/API/Program.cs
@@ -1,22 +1,28 @@
 using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
 
 Console.Clear();
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-List<Produto> produtos = new List<Produto>
-{
-    new Produto { Nome = "Notebook", Quantidade = 10, Preco = 3500.00 },
-    new Produto { Nome = "Smartphone", Quantidade = 25, Preco = 2200.00 },
-    new Produto { Nome = "Fone de Ouvido", Quantidade = 50, Preco = 199.90 },
-    new Produto { Nome = "Monitor", Quantidade = 15, Preco = 899.99 },
-    new Produto { Nome = "Teclado Mecânico", Quantidade = 20, Preco = 350.00 }
-};
+List<Produto> produtos = new List<Produto>();
+// {
+//     new Produto { Nome = "Notebook", Quantidade = 10, Preco = 3500.00 },
+//     new Produto { Nome = "Smartphone", Quantidade = 25, Preco = 2200.00 },
+//     new Produto { Nome = "Fone de Ouvido", Quantidade = 50, Preco = 199.90 },
+//     new Produto { Nome = "Monitor", Quantidade = 15, Preco = 899.99 },
+//     new Produto { Nome = "Teclado Mecânico", Quantidade = 20, Preco = 350.00 }
+// };
 
 //Funcionalidades
 //Requisições
 // - Endereço/URL
 // - Método HTTP
+
+//Respostas
+//- Código de Status
+//
 
 //Métodos HTTP
 // GET: Recupera dados de um servidor sem modificar o recurso.
@@ -28,12 +34,36 @@ app.MapGet("/", () => "API de Produtos");
 
 app.MapGet("/api/produto/listar", () =>
 {
-    return produtos;
+    if (produtos.Count > 0)
+    {
+        return Results.Ok(produtos);
+    }
+    return Results.BadRequest("Lista vazia!");
 });
 
-app.MapPost("/api/produto/cadastrar", (Produto produto) =>
+app.MapPost("/api/produto/cadastrar", ([FromBody] Produto produto) =>
 {
+    foreach (Produto produtoCadastrado in produtos)
+    {
+        if (produtoCadastrado.Nome == produto.Nome)
+        {
+            return Results.Conflict("Produto já cadastrado!");
+        }
+    }
     produtos.Add(produto);
+    return Results.Created("", produto);
+});
+
+app.MapGet("/api/produto/buscar/{nome}", ([FromRoute] string nome) =>
+{
+    //Expressão Lambda
+    Produto? produtoBuscado = produtos.FirstOrDefault(p => p.Nome == nome);
+
+    if (produtoBuscado == null)
+    {
+        return Results.NotFound("Produto não encontrado");
+    }
+    return Results.Ok(produtoBuscado);
 });
 
 app.Run();
