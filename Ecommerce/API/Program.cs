ab689496b60604bf2887a091dea39a0c9270d073
ENZO XAVIER BISCAIA <enzo.biscaia@cs.up.edu.br>
2025-09-19 09:48:55 -0300

    CRUD completo(Remover e Alterar)

--- Ecommerce/API/Program.cs
+++ Ecommerce/API/Program.cs
@@ -6,19 +6,20 @@ Console.Clear();
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-List<Produto> produtos = new List<Produto>();
-// {
-//     new Produto { Nome = "Notebook", Quantidade = 10, Preco = 3500.00 },
-//     new Produto { Nome = "Smartphone", Quantidade = 25, Preco = 2200.00 },
-//     new Produto { Nome = "Fone de Ouvido", Quantidade = 50, Preco = 199.90 },
-//     new Produto { Nome = "Monitor", Quantidade = 15, Preco = 899.99 },
-//     new Produto { Nome = "Teclado Mecânico", Quantidade = 20, Preco = 350.00 }
-// };
+List<Produto> produtos = new List<Produto>()
+{
+    new Produto { Nome = "Notebook", Quantidade = 10, Preco = 3500.00 },
+    new Produto { Nome = "Smartphone", Quantidade = 25, Preco = 2200.00 },
+    new Produto { Nome = "Fone de Ouvido", Quantidade = 50, Preco = 199.90 },
+    new Produto { Nome = "Monitor", Quantidade = 15, Preco = 899.99 },
+    new Produto { Nome = "Teclado Mecânico", Quantidade = 20, Preco = 350.00 }
+};
 
 //Funcionalidades
 //Requisições
 // - Endereço/URL
 // - Método HTTP
+// - Dados: Rota(URL) e corpo
 
 //Respostas
 //- Código de Status
@@ -30,8 +31,10 @@ List<Produto> produtos = new List<Produto>();
 // PUT: Atualiza completamente um recurso existente no servidor.
 // PATCH: Atualiza parcialmente um recurso existente.
 // DELETE: Remove um recurso do servidor.
+
 app.MapGet("/", () => "API de Produtos");
 
+//Listar produtos
 app.MapGet("/api/produto/listar", () =>
 {
     if (produtos.Count > 0)
@@ -41,6 +44,7 @@ app.MapGet("/api/produto/listar", () =>
     return Results.BadRequest("Lista vazia!");
 });
 
+//Cadastrar Produto
 app.MapPost("/api/produto/cadastrar", ([FromBody] Produto produto) =>
 {
     foreach (Produto produtoCadastrado in produtos)
@@ -54,6 +58,7 @@ app.MapPost("/api/produto/cadastrar", ([FromBody] Produto produto) =>
     return Results.Created("", produto);
 });
 
+//Buscar Produto
 app.MapGet("/api/produto/buscar/{nome}", ([FromRoute] string nome) =>
 {
     //Expressão Lambda
@@ -66,4 +71,43 @@ app.MapGet("/api/produto/buscar/{nome}", ([FromRoute] string nome) =>
     return Results.Ok(produtoBuscado);
 });
 
+//Remover Produto
+app.MapDelete("/api/produto/remover/{nome}", ([FromRoute] string nome) =>
+{
+    //Expressão Lambda
+    Produto? produtoBuscado = produtos.FirstOrDefault(p => p.Nome == nome);
+
+    if (produtoBuscado == null)
+    {
+        return Results.NotFound("Produto não encontrado");
+    }
+
+    produtos.Remove(produtoBuscado);
+
+    return Results.Ok("Produto Removido!");
+});
+
+
+//Alterar um produto pelo id
+app.MapPatch("/api/produto/alterar/{id}", ([FromRoute] string id, [FromBody] Produto produtoAlterado) =>
+{
+    //Expressão Lambda
+    Produto? produtoBuscado = produtos.FirstOrDefault(p => p.Id == id);
+
+    if (produtoBuscado == null)
+    {
+        return Results.NotFound("Produto não encontrado");
+    }
+
+    produtoBuscado.Nome = produtoAlterado.Nome;
+    produtoBuscado.Quantidade = produtoAlterado.Quantidade;
+    produtoBuscado.Preco = produtoAlterado.Preco;
+    
+
+     return Results.Ok("Produto alterado com sucesso!");
+
+});
+
+
 app.Run();
+
